633fe7267e3f307fbba41937d1bf2291a6073fe2
ATPIsTheKey <roland.fmn@gmail.com>
2019-12-16 11:50:31 +0200

    added small SDL test program

--- main.c
+++ main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
+
 #include "catastrophic-chip8/chip8.h"
 
 #define CLOCK_SPEED 500
 
-int main() {
+// Small test program to make sure SDL2 library is working
+int main(int argc, char *argv[])
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow(
+            "SDL2Test",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            640,
+            480,
+            0
+    );
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
