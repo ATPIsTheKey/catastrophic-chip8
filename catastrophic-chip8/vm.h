b503a0928c6966480b06abdbc841da8876a2e583
ATPIsTheKey <roland.fmn@gmail.com>
2019-12-20 22:53:37 +0200

    changed naming convention

--- catastrophic-chip8/vm.h
+++ catastrophic-chip8/vm.h
@@ -2,29 +2,30 @@
 // Created by roland on 2019-12-14.
 //
 
-#ifndef CATASTROPHIC_CHIP8_VM_H
-#define CATASTROPHIC_CHIP8_VM_H
+#ifndef CATASTROPHIC_CH8_VM_H
+#define CATASTROPHIC_CH8_VM_H
 
 #include <stdint.h>
 #include <stddef.h>
 
-enum CHIP8VM_SYSCONSTANTS {
+enum CH8VM_SYSCONSTANTS {
     SCR_W        = 64,
     SCR_H        = 32,
     FONTSET_SIZE = 80,
     MEM_SIZE     = 4096 // 0xFFF
 };
 
-enum CHIP8VM_SYSFLAGS{
+enum CH8VM_GENERAL_FLAGS {
     DRAWFLAG = 1u << 0u
 };
+
 
 typedef uint8_t  register8_t;
 typedef uint16_t register16_t;
 
-typedef struct _CHIP8_CPU {
+typedef struct _CH8_CPU {
     /* CHIP-8 has 16 8-bit wide registers, V0 to VF*/
-    register8_t  VX[16];
+    register8_t  Vx[16];
     /* CHIP-8 has a 16-bit address register, I, which is 16 bits wide and is used
      * with several opcodes that involve memory operations. */
     register16_t I;
@@ -43,30 +44,29 @@ typedef struct _CHIP8_CPU {
      * subroutines are called. */
     register8_t  sp;
     uint16_t stack[16];
+} CH8_CPU;
 
-} CHIP8_CPU;
 
-
-typedef struct _CHIP8_VM {
-    CHIP8_CPU *cpu;
+typedef struct _CH8_VM {
+    CH8_CPU *cpu;
 
     uint8_t mem[MEM_SIZE];
     uint8_t framebuffer[SCR_W * SCR_H];
-    uint8_t keypad[16]; // 16-key hexadecimal keypad
+    uint16_t keypad[16]; // state of 16-key hexadecimal keypad
 
     uint16_t current_opcode;
     uint32_t cycle_cnt; // number of cpu cycles mod clock frequency
     uint32_t n_cycles_timerdecr; // cpu cycles per timer register decrement
 
     uint32_t flags;
-} CHIP8_VM;
+} CH8_VM;
 
 
-CHIP8_VM *CHIP8VM_init(size_t clockspeed);
-void      CHIP8VM_kill(CHIP8_VM *vm);
-void      CHIP8VM_load_rom(CHIP8_VM *vm, char *fpath);
-void      CHIP8VM_emulate_cycle(CHIP8_VM *vm);
-int       CHIP8VM_is_drawflag_set(CHIP8_VM *vm);
-void      CHIP8VM_set_keys(uint8_t *keyboard_state);
+CH8_VM *CH8VM_init(size_t clockspeed);
+void    CH8VM_kill(CH8_VM *vm);
+void    CH8VM_load_rom(CH8_VM *vm, char *fpath);
+void    CH8VM_emulate_cycle(CH8_VM *vm);
+int     CH8VM_is_drawflag_set(CH8_VM *vm);
+void    CH8VM_set_keys(uint8_t *keyboard_state);
 
-#endif //CATASTROPHIC_CHIP8_VM_H
+#endif //CATASTROPHIC_CH8_VM_H
