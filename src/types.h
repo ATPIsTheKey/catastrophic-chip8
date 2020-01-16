469bcb716dc90ddcdbf1c63d0212e0365831c434
ATPIsTheKey <roland.fmn@gmail.com>
2020-01-16 16:08:53 +0200

    split type declarations into separate file

---
+++ src/types.h
@@ -0,0 +1,13 @@
+//
+// Created by roland on 2020-01-15.
+//
+
+#ifndef CATASTROPHIC_CHIP8_TYPES_H
+#define CATASTROPHIC_CHIP8_TYPES_H
+
+#include <stddef.h>
+
+typedef uint8_t  reg8_t;
+typedef uint16_t reg16_t;
+
+#endif //CATASTROPHIC_CHIP8_TYPES_H
